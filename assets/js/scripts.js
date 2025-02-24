f23d22fb434d70f11b2fb348f5df430fc35f674b
Riivveera <113799396+Riivveera@users.noreply.github.com>
2025-02-23 23:45:33 -0600

    turn around x

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Polaroid Memories Website Loaded!");
+function flipCard(event, el) {
+    if (event.stopPropagation) event.stopPropagation();
+    const polaroid = el.closest('.polaroid');
+    polaroid.classList.toggle('flipped');
+}
+
+document.querySelectorAll('.polaroid').forEach(card => {
+    card.addEventListener('click', function() {
+        this.classList.add('flipped');
+    });
+});
